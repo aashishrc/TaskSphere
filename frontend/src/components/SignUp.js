d73ba8785b397cbf3f333dc590aecef3261c0925
akhil8951 <kumbar.a@northeastern.edu>
2023-12-10 20:25:49 -0500

    integration Trial

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/css/SignUp.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+// import baseUrl from process.env.BASE_URL;
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    role: "",
+  });
+
+  const handleChange = (e) => {
+    console.log("changed");
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/v1/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Signup successful:", data);
+        // Handle successful signup, e.g., redirect to login page
+      } else {
+        const errorData = await response.json();
+        console.error("Signup failed:", errorData);
+        // Handle signup failure, e.g., show an error message
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+      // Handle network errors or other exceptions
+    }
+  };
+
   return (
     <>
       <Container fluid className="signUp-main">
@@ -14,7 +58,7 @@ const SignUp = () => {
         <Container className="signUp-form">
           <Row className="justify-content-md-center">
             <Col md="10">
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <Row>
                   <Col>
                     <Form.Group
@@ -25,7 +69,10 @@ const SignUp = () => {
                       <Form.Control
                         type="text"
                         placeholder="Enter your First Name"
+                        value={formData.firstName}
+                        onChange={handleChange}
                       />
+                      {/* <input type="text"></input> */}
                     </Form.Group>
                   </Col>
                   <Col>
@@ -37,6 +84,8 @@ const SignUp = () => {
                       <Form.Control
                         type="text"
                         placeholder="Enter your Last Name"
+                        value={formData.lastName}
+                        onChange={handleChange}
                       />
                     </Form.Group>
                   </Col>
@@ -48,10 +97,12 @@ const SignUp = () => {
                       controlId="formBasicEmail"
                       className="formGroup-custom"
                     >
-                      <Form.Label>Email Id :</Form.Label>
+                      <Form.Label>UserName :</Form.Label>
                       <Form.Control
                         type="email"
                         placeholder="Enter your email Id"
+                        value={formData.username}
+                        onChange={handleChange}
                       />
                     </Form.Group>
                   </Col>
@@ -63,7 +114,7 @@ const SignUp = () => {
                       <Form.Label>Choose your Role : </Form.Label>
                       <Form.Select>
                         <option>Manager</option>
-                        <option>Tech Lead</option>
+                        <option> Tech Lead</option>
                         <option>Developer</option>
                       </Form.Select>
                     </Form.Group>
@@ -81,26 +132,15 @@ const SignUp = () => {
                       <Form.Control
                         type="password"
                         placeholder="Enter your Password"
-                      />
-                    </Form.Group>
-                  </Col>
-                  <Col>
-                    <Form.Group
-                      controlId="formBasicMatchPassword"
-                      className="formGroup-custom"
-                    >
-                      <Form.Label>Re-type Password :</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Enter your Password again"
+                        value={formData.password}
+                        onChange={handleChange}
                       />
                     </Form.Group>
                   </Col>
                 </Row>
-
                 <br />
 
-                <Button variant="secondary" type="button">
+                <Button variant="secondary" type="submit">
                   Create Account
                 </Button>
               </Form>
